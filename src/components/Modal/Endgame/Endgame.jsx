f88da10e6b09d3498e968436a54295f419fdd2ca
paulcourtois <paulcourtoispaul@gmail.com>
2022-07-18 16:12:30 +0200

    style: Fix indent in score

--- src/components/Modal/Endgame/Endgame.jsx
+++ src/components/Modal/Endgame/Endgame.jsx
@@ -18,21 +18,28 @@ const Endgame = ({
   <S.EndgameTitle>
     {gameState === "victory" ? t('endgame.win') : t('endgame.loss')}
   </S.EndgameTitle>
-  {gameState === "victory" 
-  ? <S.EndgameText>
-  {t('endgame.foundIn')} <span>{t('endgame.numberOfTries_interval', {postProcess: 'interval',count: (selectedAttempt + 1)})}</span> !
- </S.EndgameText>
-  :<S.EndgameText>
-    {t('endgame.doBetter')}  
-  </S.EndgameText> }
+    {gameState === "victory" 
+    ? <S.EndgameText>
+      {t('endgame.foundIn')} <span>{t('endgame.numberOfTries_interval', {postProcess: 'interval',count: (selectedAttempt + 1)})}</span> !
+    </S.EndgameText>
+    :<S.EndgameText>
+      {t('endgame.doBetter')}  
+    </S.EndgameText> 
+    }
    <S.EndgameText>
-   {t('endgame.wordWas')}<span>{wordsHelper.setWordOfTheDay().basqueWord.toUpperCase()}</span>.
+    {t('endgame.wordWas')}
+    <span>
+      {wordsHelper.setWordOfTheDay().basqueWord.toUpperCase()}
+    </span>.
    </S.EndgameText>
    <S.EndgameText>
-   {t('endgame.basqueTranslation')}<span>{(language === 'french') 
-   ? wordsHelper.setWordOfTheDay().frenchTranslation.toUpperCase()
-   : wordsHelper.setWordOfTheDay().spanishTranslation.toUpperCase()
-  }</span>.
+    {t('endgame.basqueTranslation')}
+    <span>
+      {(language === 'french') 
+      ? wordsHelper.setWordOfTheDay().frenchTranslation.toUpperCase()
+      : wordsHelper.setWordOfTheDay().spanishTranslation.toUpperCase()
+      }
+    </span>.
    </S.EndgameText>
    <Score t={t} selectedAttempt={selectedAttempt}/>
   </>;
--- src/components/Modal/Endgame/Score/Score.jsx
+++ src/components/Modal/Endgame/Score/Score.jsx
@@ -8,7 +8,7 @@ const Score = ({t, selectedAttempt}) => {
   const currentScore = typeof window !== 'undefined' && JSON.parse(localStorage.getItem('score'));
   const numberOfGames = currentScore.reduce(
     (previousValue, currentValue) => previousValue + currentValue
-    );
+  );
   const subtitle = t('endgame.victory_interval', {postProcess: 'interval',count: (numberOfGames - currentScore[6])}) 
   + ' / '
   + t('endgame.defeat_interval', {postProcess: 'interval',count: (currentScore[6])})
@@ -19,18 +19,25 @@ const Score = ({t, selectedAttempt}) => {
     <S.EndgameText>
       {subtitle}
     </S.EndgameText>
-
     {currentScore.map((score,index )=>{
-          const winPercent = (score / numberOfGames)*100
+      const winPercent = (score / numberOfGames)*100
+
       return <S.Scoreline key={index}>
-        <p>{index === 6 ? 'X' : index + 1}</p>
-        <S.ScoreBar 
-        index={index} 
-        winPercent={winPercent} 
-        title={`${parseInt(winPercent)}%`}
-        currentGameResult={(index === 6 && localStorage.getItem('gameState') === 'loss') || (localStorage.getItem('gameState') !== 'loss' && index === selectedAttempt)}
-        >
-          <div><S.Score>{score}</S.Score></div>
+          <p>
+            {index === 6 ? 'X' : index + 1}
+          </p>
+          <S.ScoreBar 
+            index={index} 
+            winPercent={winPercent} 
+            title={`${parseInt(winPercent)}%`}
+            currentGameResult={(index === 6 && localStorage.getItem('gameState') === 'loss') 
+            || (localStorage.getItem('gameState') !== 'loss' && index === selectedAttempt)}
+          >
+          <div>
+            <S.Score>
+              {score}
+            </S.Score>
+          </div>
         </S.ScoreBar>
       </S.Scoreline>
     })}
